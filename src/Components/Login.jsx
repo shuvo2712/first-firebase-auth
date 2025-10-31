0ea44e9251a9e323dea46a9aec4eb78b7f59c6c0
shuvo2712 <shuvo2712@gmail.com>
2025-10-31 23:33:28 +0600

    google login popup

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../Firebase/firebase.config";
+
+const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login.jsx</h1>
-        </div>
-    );
+  const handleLoginGoogle = () => {
+    console.log("Google login button clicked");
+
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log("user", result.user);
+      })
+      .catch((error) => {
+        console.log("Error during Google sign-in:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <button onClick={handleLoginGoogle}>Login by Google</button>
+    </div>
+  );
 };
 
 export default Login;
