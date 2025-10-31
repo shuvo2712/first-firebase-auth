5cbf323fa7b45716615d17041ae566bf0121c9e9
shuvo2712 <shuvo2712@gmail.com>
2025-11-01 00:38:33 +0600

    toggle signIn signOut

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,26 +1,60 @@
 import React from "react";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../Firebase/firebase.config";
 
 const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
-  const handleLoginGoogle = () => {
+  const [user, setUser] = React.useState(null);
+
+  // Sign In by google
+  const handleSignInGoogle = () => {
     console.log("Google login button clicked");
 
     signInWithPopup(auth, googleProvider)
       .then((result) => {
-        console.log("user", result.user);
+        console.log("USER LOGIN :", result.user);
+        setUser(result.user);
       })
       .catch((error) => {
-        console.log("Error during Google sign-in:", error);
+        console.log("ERROR :", error);
+      });
+  };
+
+  // Sign Out
+  const handleSignOut = () => {
+    console.log("Sign out button clicked");
+
+    signOut(auth)
+      .then(() => {
+        console.log(user.displayName, "signed out successfully");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log("ERROR :", error);
       });
   };
 
   return (
     <div>
-      <h1>Login Page</h1>
-      <button onClick={handleLoginGoogle}>Login by Google</button>
+      <h1>Login</h1>
+
+      {/* if no user */}
+      {!user && (
+        <div className="login">
+          <button onClick={handleSignInGoogle}>Login by Google</button>
+        </div>
+      )}
+
+      {/* if user  */}
+      {user && (
+        <div className="login">
+          <h3>{user?.displayName}</h3>
+          <img src={user?.photoURL} alt="" />
+          <h3>{user?.email}</h3>
+          <button onClick={handleSignOut}>Sign Out</button>
+        </div>
+      )}
     </div>
   );
 };
